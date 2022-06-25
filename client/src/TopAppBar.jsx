246fea880629b1f7c7b8e0d03d6bc9f46485e8fb
Darios Pachtsinis <dapait03@hs-esslingen.de>
2022-06-25 15:26:03 +0200

    Deletes

--- client/src/TopAppBar.jsx
+++ client/src/TopAppBar.jsx
@@ -57,33 +57,18 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const Posts = () => {
-	const [data, setDate] = React.useState([]);
-	const [ownPosts, setOwnPosts] = React.useState(false);
-	const [noPosition, setNoPosition] = React.useState(false);
-	const [loadingPosts, setLoadingPosts] = React.useState(true);
+const TopAppBar = () => {
 	const [anchorEl, setAnchorEl] = React.useState(null);
-	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-	const [headerText, setHeaderText] = React.useState('User');
   
 	const isMenuOpen = Boolean(anchorEl);
-	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
   
 	const handleProfileMenuOpen = (event) => {
 	  setAnchorEl(event.currentTarget);
 	};
   
-	const handleMobileMenuClose = () => {
-	  setMobileMoreAnchorEl(null);
-	};
-  
 	const handleMenuClose = () => {
 	  setAnchorEl(null);
 	  handleMobileMenuClose();
-	};
-  
-	const handleMobileMenuOpen = (event) => {
-	  setMobileMoreAnchorEl(event.currentTarget);
 	};
   
 	const menuId = 'primary-search-account-menu';
@@ -105,58 +90,6 @@ const Posts = () => {
 	  >
 		<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
 		<MenuItem onClick={handleMenuClose}>My account</MenuItem>
-	  </Menu>
-	);
-  
-	const mobileMenuId = 'primary-search-account-menu-mobile';
-	const renderMobileMenu = (
-	  <Menu
-		anchorEl={mobileMoreAnchorEl}
-		anchorOrigin={{
-		  vertical: 'top',
-		  horizontal: 'right',
-		}}
-		id={mobileMenuId}
-		keepMounted
-		transformOrigin={{
-		  vertical: 'top',
-		  horizontal: 'right',
-		}}
-		open={isMobileMenuOpen}
-		onClose={handleMobileMenuClose}
-	  >
-		<MenuItem>
-		  <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-			<Badge badgeContent={4} color="error">
-			  <MailIcon />
-			</Badge>
-		  </IconButton>
-		  <p>Messages</p>
-		</MenuItem>
-		<MenuItem>
-		  <IconButton
-			size="large"
-			aria-label="show 17 new notifications"
-			color="inherit"
-		  >
-			<Badge badgeContent={17} color="error">
-			  <NotificationsIcon />
-			</Badge>
-		  </IconButton>
-		  <p>Notifications</p>
-		</MenuItem>
-		<MenuItem onClick={handleProfileMenuOpen}>
-		  <IconButton
-			size="large"
-			aria-label="account of current user"
-			aria-controls="primary-search-account-menu"
-			aria-haspopup="true"
-			color="inherit"
-		  >
-			<LogoutIcon />
-		  </IconButton>
-		  <p>Profile</p>
-		</MenuItem>
 	  </Menu>
 	);
 
@@ -187,9 +120,6 @@ const Posts = () => {
             />
           </Search>
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-
-
-
             <IconButton
               size="large"
               edge="end"
@@ -210,25 +140,11 @@ const Posts = () => {
                 <LogoutIcon />
               </Badge>
             </IconButton>
-
           </Box>
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-
           </Box>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
       {renderMenu}
     </Box>
   )
