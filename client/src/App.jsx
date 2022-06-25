ab4fa49f0dcfa308c2a00cd2deeee5f3a703759e
Darios Pachtsinis <dapait03@hs-esslingen.de>
2022-06-25 17:22:27 +0200

    Wichtiger Push!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,7 +42,6 @@ class App extends React.Component {
 				<div>
 					<TopAppBar />
 					<SideMenu />
-					<Customer />
 					<Routes>
 						<Route path="/" element={<Customer />} />
 						<Route path="/users" element={<User />} />
