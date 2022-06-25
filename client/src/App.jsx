e1d7320ba9b9be7dbee649cca345b879670ae3ad
Darios Pachtsinis <dapait03@hs-esslingen.de>
2022-06-25 15:13:39 +0200

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { withStyles } from '@material-ui/core/styles';
 
 import Login from './Login';
-import Posts from './Posts'
+import Customer from './Customer';
+import User from './User';
+import Contract from './Contract';
+
+import { Routes, Route, Link } from "react-router-dom";
 
 const styles = theme => ({
 	center: {
@@ -31,7 +35,16 @@ class App extends React.Component {
 	render() {
 		if (this.state.loggedIn) {
 			return (
-			<Posts url={theUrl} ></Posts> 
+				<div>
+					<Button component={Link} to="/" variant="contained" color="primary">Customers</Button>
+					<Button component={Link} to="/users" variant="contained" color="primary">Users</Button>
+					<Button component={Link} to="/contracts" variant="contained" color="primary">Contracts</Button>
+					<Routes>
+						<Route path="/" element={<Customer />} />
+						<Route path="/users" element={<User />} />
+						<Route path="/contracts" element={<Contract />} />
+					</Routes>
+				</div>
 			);
 		} else {
 			return (
