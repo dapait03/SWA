08ec1b23e65bbc472211ba8bb628e22ea872047b
rihoit00 <rihoit00@hs-esslingen.de>
2022-06-25 14:52:43 +0200

    Posts.jsx

--- client/src/Posts.jsx
+++ client/src/Posts.jsx
@@ -1,42 +1,239 @@
-import React from "react";
-import { withStyles } from '@material-ui/core/styles';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import LogoutIcon from '@mui/icons-material/Logout';
+import MailIcon from '@mui/icons-material/Mail';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import Button from '@mui/material/Button';
 
-import PostList from './PostList'
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
 
-const styles = theme => ({
-	center: {
-		display: 'block',
-		justifyContent: 'center',
-		alignItems: 'center',
-		minHeight: 'calc(100vh - 40px)',
-		border: '5px solid red',
-		padding: '10px',
-	}
-});
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
-class Posts extends React.Component {
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
-	constructor(props) {
-		super(props);
-	    this.state = {	  
-            data: [],
-            ownPosts: false,
-            noPosition: false,
-            loadingPosts: true
-		};		
-	}
-	
+const Posts = () => {
+	const [data, setDate] = React.useState([]);
+	const [ownPosts, setOwnPosts] = React.useState(false);
+	const [noPosition, setNoPosition] = React.useState(false);
+	const [loadingPosts, setLoadingPosts] = React.useState(true);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+	const [headerText, setHeaderText] = React.useState('User');
   
-	render() {
-		const { classes } = this.props;
-		return (
-		<div className={classes.center}>
-			<h1>Here goes the posts list plus buttons for adding and editing posts</h1>
-			<PostList></PostList>
-	    </div>
-		);
-	}
+	const isMenuOpen = Boolean(anchorEl);
+	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  
+	const handleProfileMenuOpen = (event) => {
+	  setAnchorEl(event.currentTarget);
+	};
+  
+	const handleMobileMenuClose = () => {
+	  setMobileMoreAnchorEl(null);
+	};
+  
+	const handleMenuClose = () => {
+	  setAnchorEl(null);
+	  handleMobileMenuClose();
+	};
+  
+	const handleMobileMenuOpen = (event) => {
+	  setMobileMoreAnchorEl(event.currentTarget);
+	};
+  
+	const menuId = 'primary-search-account-menu';
+	const renderMenu = (
+	  <Menu
+		anchorEl={anchorEl}
+		anchorOrigin={{
+		  vertical: 'top',
+		  horizontal: 'right',
+		}}
+		id={menuId}
+		keepMounted
+		transformOrigin={{
+		  vertical: 'top',
+		  horizontal: 'right',
+		}}
+		open={isMenuOpen}
+		onClose={handleMenuClose}
+	  >
+		<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+		<MenuItem onClick={handleMenuClose}>My account</MenuItem>
+	  </Menu>
+	);
+  
+	const mobileMenuId = 'primary-search-account-menu-mobile';
+	const renderMobileMenu = (
+	  <Menu
+		anchorEl={mobileMoreAnchorEl}
+		anchorOrigin={{
+		  vertical: 'top',
+		  horizontal: 'right',
+		}}
+		id={mobileMenuId}
+		keepMounted
+		transformOrigin={{
+		  vertical: 'top',
+		  horizontal: 'right',
+		}}
+		open={isMobileMenuOpen}
+		onClose={handleMobileMenuClose}
+	  >
+		<MenuItem>
+		  <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+			<Badge badgeContent={4} color="error">
+			  <MailIcon />
+			</Badge>
+		  </IconButton>
+		  <p>Messages</p>
+		</MenuItem>
+		<MenuItem>
+		  <IconButton
+			size="large"
+			aria-label="show 17 new notifications"
+			color="inherit"
+		  >
+			<Badge badgeContent={17} color="error">
+			  <NotificationsIcon />
+			</Badge>
+		  </IconButton>
+		  <p>Notifications</p>
+		</MenuItem>
+		<MenuItem onClick={handleProfileMenuOpen}>
+		  <IconButton
+			size="large"
+			aria-label="account of current user"
+			aria-controls="primary-search-account-menu"
+			aria-haspopup="true"
+			color="inherit"
+		  >
+			<LogoutIcon />
+		  </IconButton>
+		  <p>Profile</p>
+		</MenuItem>
+	  </Menu>
+	);
+
+  return (
+	<Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            {headerText}
+          </Typography>
+
+		  <Button variant="outlined">Add</Button>
+
+		  <Box sx={{ flexGrow: 1 }} />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Filter…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+
+
+
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircleIcon />
+            </IconButton>
+
+			<IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge>
+                <LogoutIcon />
+              </Badge>
+            </IconButton>
+
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  )
 }
 
-export default withStyles(styles)(Posts);
+export default Posts
 
