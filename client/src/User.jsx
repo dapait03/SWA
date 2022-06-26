082aee6ef930a558ad0aef5f8fa9a4e3503fdf67
Darios Pachtsinis <dapait03@hs-esslingen.de>
2022-06-26 16:50:08 +0200

    User.jsx

--- client/src/User.jsx
+++ client/src/User.jsx
@@ -21,29 +21,32 @@ class User extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            customers: [],
+            users: [],
         };
     }
 
 
 
     componentDidMount() {
-        HttpService.getCustomers().then(res => {
-            this.setState({ customers : res });
+        HttpService.getUsers().then(res => {
+            this.setState({ users : res });
         });
     }
 
     render() {
         const { classes } = this.props;
         return (
-            <div className={classes.center}>
-                <Button variant="contained" color="primary" >Add Customer </Button>
-                <h1>Customers </h1>
-
-                {this.state.customers&& this.state.customers.map((cus, i) =>
-                 <Box key={i}>{cus.name}</Box>
+            <div >
+                {this.state.users&& this.state.users.map((users) =>
+                <table style={{background: "#1976D2", width:"100%"}}>
+                    <tr className="tableRow" key={users.custID} style={{background: "#1986D2", width:"100%"}}>
+                        <td className="tableCell" style={{border: "1px solid grey", width: "4%"}}>{users.userFirstName}{users.userLastName}</td>
+                        <td className="tableCell" style={{border: "1px solid grey", width: "46%"}}>{users.userMail}</td>
+                        <td className="tableCell" style={{border: "1px solid grey", width: "25%"}}>{users.userPhoneNumber1}</td>
+                        <Button sx={{margin: "10px"}}variant="contained" color="primary" onClick={() => this.openDialog()}>Edit</Button>
+                    </tr>
+                </table>
                 )}
-
             </div>
         );
     }
