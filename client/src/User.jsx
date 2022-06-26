484193bc0b9af5879b1f5495699811df28818b1b
Darios Pachtsinis <dapait03@hs-esslingen.de>
2022-06-26 17:24:09 +0200

    Style

--- client/src/User.jsx
+++ client/src/User.jsx
@@ -55,8 +55,8 @@ class User extends React.Component {
                 {this.state.users&& this.state.users.map((users) =>
                 <table style={{width:"100%"}}>
                     <tr className="tableRow" key={users.custID} style={{width:"100%"}}>
-                        <td className="tableCell" style={{border: "2px solid grey", width: "4%"}}>{users.userFirstName}{users.userLastName}</td>
-                        <td className="tableCell" style={{border: "2px solid grey", width: "46%"}}>{users.userMail}</td>
+                        <td className="tableCell" style={{border: "2px solid grey", width: "8%"}}>{users.userFirstName}{users.userLastName}</td>
+                        <td className="tableCell" style={{border: "2px solid grey", width: "42%"}}>{users.userMail}</td>
                         <td className="tableCell" style={{border: "2px solid grey", width: "25%"}}>{users.userPhoneNumber1}</td>
                         <Button sx={{margin: "10px"}}variant="contained" color="primary" onClick={this.openDialog}>Edit</Button>
                     </tr>
